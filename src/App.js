0a5d9d7bbbe9ec105b61dcd7e98fb3079e7af260
Guilherme Fernandes <72757771+FernandesGuilherme07@users.noreply.github.com>
2021-10-20 11:53:06 -0300

    component of class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      name: "Guilherme Fernandes",
+      counter: 0
+    };
+  }
+
+  handlePClick = () => {
+    this.setState({ name: 'Silvano' })
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() {
+    const { name, counter } = this.state;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onclick={this.handlePClick}>
+          {name} {counter}
         </p>
         <a
           className="App-link"
@@ -20,6 +43,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
