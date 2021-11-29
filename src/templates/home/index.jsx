13568505b94fd9461b2c63b3f1d855a0cd140f31
Guilherme Fernandes <72757771+FernandesGuilherme07@users.noreply.github.com>
2021-11-29 10:38:44 -0300

    textInput

--- src/templates/home/index.jsx
+++ src/templates/home/index.jsx
@@ -3,6 +3,7 @@ import './styles.css';
 import { Posts } from '../../components/posts'
 import { loadposts } from '../../utils/load-posts';
 import { Buttom } from '../../components/Buttom';
+import { TextInput } from '../../components/TextInput';
 
 class Home extends Component {
  state = {
@@ -10,6 +11,7 @@ class Home extends Component {
       allPosts: [],
       page: 0,
       postsPerPage: 9,
+      searchValue: '',
     };
 
     async componentDidMount() {
@@ -39,19 +41,54 @@ class Home extends Component {
 
     this.setState({ posts, page: nextPage });
   }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({searchValue: value})
+  }
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ?
+    allPosts.filter(post => {
+      return post.title.toLowerCase().includes(
+        searchValue.toLowerCase()
+      );
+    })
+    : posts;
 
     return (
 
       <section className="container">
-        <Posts posts={posts}/>
-        <Buttom 
+        <div class="search-container">
+          {!!searchValue && (
+            <h1>Search value: {searchValue}</h1>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
+        <br/> <br/> <br/>
+
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
+        {filteredPosts.length === 0 && (
+          <p>Não existem posts =(</p>
+        )}
+
+        <div className="buttom-container">
+          {!searchValue&& (
+             <Buttom 
          text="Load more posts"
          onClick={this.loadMorePosts}
          disabled={noMorePosts}
         />
+          )}
+       
+        </div>
       </section>
     );
 }
